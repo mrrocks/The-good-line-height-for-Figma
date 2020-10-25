dc3525e59796bc8d8c3045a86016b53eac3103bc
Fran Perez <mr.rockss@gmail.com>
2020-10-25 18:07:35 +0100

    First functional version

--- code.js
+++ code.js
@@ -1,13 +1,4 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-// // Calls to "parent.postMessage" from within the HTML page will trigger this
-// // callback. The callback will be passed the "pluginMessage" property of the
-// // posted message.
 // figma.ui.onmessage = msg => {
 //   // One way of distinguishing between different types of messages sent from
 //   // your HTML page is to use an object with a "type" property like this.
@@ -27,13 +18,19 @@ figma.showUI(__html__);
 //   // keep running, which shows the cancel button at the bottom of the screen.
 //   figma.closePlugin();
 // };
-figma.ui.onmessage = msg => {
-    // if (msg.type === 'create-rectangles') {
-    for (const node of figma.currentPage.selection) {
-        if ("opacity" in node) {
-            node.opacity = 0.5;
-        }
-    }
-    console.log("yo");
-    // }
+// figma.loadFontAsync({ family: "Roboto", style: "Regular" })
+let calculateLineHeight = (size, multiplier, grid) => {
+    let lineHeight = Math.ceil((size * multiplier) / grid) * grid;
+    return lineHeight;
+};
+let updateSelection = (event) => {
+    const node = figma.currentPage.selection[0];
+    figma.loadFontAsync(node.fontName).then(() => {
+        const newLineHeight = Object.assign({}, node.lineHeight);
+        newLineHeight.value = calculateLineHeight(node.fontSize, event.multiplier, event.grid);
+        node.lineHeight = newLineHeight;
+    });
+};
+figma.ui.onmessage = (event) => {
+    updateSelection(event);
 };
