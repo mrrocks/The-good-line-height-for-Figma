adcc80e089b69c85087ecde3d52fcab985716804
Fran Perez <franperez@Frans-MacBook-Pro.local>
2020-10-26 20:56:25 +0100

    Finishing touches

--- code.js
+++ code.js
@@ -1,31 +1,12 @@
 figma.showUI(__html__);
 figma.ui.resize(240, 168);
-// figma.ui.onmessage = msg => {
-//   // One way of distinguishing between different types of messages sent from
-//   // your HTML page is to use an object with a "type" property like this.
-//   if (msg.type === 'create-rectangles') {
-//     const nodes: SceneNode[] = [];
-//     for (let i = 0; i < msg.count; i++) {
-//       const rect = figma.createRectangle();
-//       rect.x = i * 150;
-//       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-//       figma.currentPage.appendChild(rect);
-//       nodes.push(rect);
-//     }
-//     figma.currentPage.selection = nodes;
-//     figma.viewport.scrollAndZoomIntoView(nodes);
-//   }
-//   // Make sure to close the plugin when you're done. Otherwise the plugin will
-//   // keep running, which shows the cancel button at the bottom of the screen.
-//   figma.closePlugin();
-// };
-// figma.loadFontAsync({ family: "Roboto", style: "Regular" })
+let selection = figma.currentPage.selection;
 let calculateLineHeight = (size, multiplier, grid) => {
     let lineHeight = Math.ceil((size * multiplier) / grid) * grid;
     return lineHeight;
 };
 let updateSelection = (event) => {
-    const selection = figma.currentPage.selection;
+    selection = figma.currentPage.selection;
     for (let el of selection) {
         figma.loadFontAsync(el.fontName).then(() => {
             let newLineHeight = Object.assign({}, el.lineHeight);
@@ -34,6 +15,17 @@ let updateSelection = (event) => {
         });
     }
 };
+let enableButton = () => { };
+let disableButton = () => { };
+figma.on("selectionchange", () => {
+    selection = figma.currentPage.selection;
+    if (selection.length == 0) {
+        console.log("nothing selected");
+    }
+    else if (selection.length > 0 && selection.every(node => node.type == "TEXT")) {
+        console.log(selection);
+    }
+});
 figma.ui.onmessage = (event) => {
     updateSelection(event);
 };
